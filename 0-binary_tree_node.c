2efe01bccbed7896337599a86a5dcbe6760b6b53
Yemi-21 <yemsrachbeza@gmail.com>
2022-12-13 00:36:32 -0800

    Binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,23 +2,18 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * 
  * @parent: parent node
  * @value: node value
- *
  * Return: new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
-
 	if (new == NULL)
 		return (NULL);
-
 	new->parent = parent;
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
-
 	return (new);
 }
